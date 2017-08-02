3e856af18e93af9f45eab7b5384464b4c844c468
Austen Lopez <austen@Austens-MacBook.local>
2017-08-02 10:44:43 -0500

    made chages to javascript code

--- austen_why/javascriptProject.js
+++ austen_why/javascriptProject.js
@@ -1,38 +1,4 @@
 
-
-// function eatASentence(paragraph) {
-//   indexOfFirstPeriod = paragraph.indexOf(".");
-//   firstSentence = paragraph.slice(0, indexOfFirstPeriod);
-//   return paragraph.slice(indexOfFirstParagraph +2, paragraph.length);
-//
-// }
-//
-// function countSentences(paragraph) {
-//   var sentence = 0;
-//   while (paragraph.length > 0){
-//     paragraph = eatASentence(paragraph);
-//     sentence++;
-//
-//   }
-//   return sentence;
-// }
-//
-//
-// function eatAWord (paragraph) {
-//   indexOfSpace = paragraph.indexOf(" ");
-//   firstWord = paragraph.slice(0, indexOfFirstSpace);
-//   return paragraph.slice(indexofFirstParagraph +1, paragraph.length);
-//
-// }
-//
-// function countWords (paragraph){
-//   var word = 0;
-//   while (paragraph.length > 0) {
-//     paragraph = eatAWord(paragraph);
-//     word++;
-//
-//   }
-// }
 var button = document.getElementById("myButton");
 button.addEventListener('click',function(){
  var paragraph = document.getElementById("paragraph").value;
