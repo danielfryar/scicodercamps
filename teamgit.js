ced66927c347097f3ced74c72cd48202b91e6ec1
Matthew Colvard <mustangf350@yahoo.com>
2018-03-06 12:17:17 -0600

    do it now

--- teamgit.js
+++ teamgit.js
@@ -1,3 +1,3 @@
 document.body.style.backgroundColor = "green";
 console.log("Hello teamgit, welcome back again...!");
-console.log("and once again welcome back and remember to breath!");
+console.log("and once again welcome back and remember to breathe!");
